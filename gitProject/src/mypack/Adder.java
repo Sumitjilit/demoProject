5543f376e8d80c9b023eed7d65b24fe992b6ef54
Neeraj <nraj_adi@yahoo.co.in>
2018-03-06 22:16:37 +0530

    Adder.java is added and Demo.java is modified.

---
+++ gitProject/src/mypack/Adder.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class Adder {
+
+	public static int add(int x, int y)
+	{
+		return x+y;
+	}
+}
--- gitProject/src/mypack/Demo.java
+++ gitProject/src/mypack/Demo.java
@@ -1,9 +1,17 @@
 package mypack;
 
+import java.util.Scanner;
+
 public class Demo {
 
 	public static void main(String arr[])
 	{
-		System.out.println("It is a git project.");
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter first no.");
+		int x=in.nextInt();
+		System.out.print("Enter second no.");
+		int y=in.nextInt();
+		int z=Adder.add(x, y);
+		System.out.println("Sum is: "+z);
 	}
 }
