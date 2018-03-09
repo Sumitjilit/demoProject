d7e916061c04f417e9e4d56cb5bd1e53332029ec
Neeraj <nraj_adi@yahoo.co.in>
2018-03-09 10:29:30 +0530

    Demo.java is changed to display the sum of constants.

--- gitProject/src/mypack/Demo.java
+++ gitProject/src/mypack/Demo.java
@@ -1,17 +1,12 @@
 package mypack;
 
-import java.util.Scanner;
 
 public class Demo {
 
 	public static void main(String arr[])
 	{
-		Scanner in=new Scanner(System.in);
-		System.out.print("Enter first no.");
-		int x=in.nextInt();
-		System.out.print("Enter second no.");
-		int y=in.nextInt();
-		int z=Adder.add(x, y);
+		System.out.println("Adding 4 and 5...");
+		int z=Adder.add(4, 5);
 		System.out.println("Sum is: "+z);
 	}
 }
