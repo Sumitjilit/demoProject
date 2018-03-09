87447d994a596fca7b10f72cc404cb9a0fc090ce
Neeraj <nraj_adi@yahoo.co.in>
2018-03-09 10:38:30 +0530

    Demo.java is changed again.

--- gitProject/src/mypack/Demo.java
+++ gitProject/src/mypack/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
 
 	public static void main(String arr[])
 	{
-		System.out.println("Adding 4 and 5...");
-		int z=Adder.add(4, 5);
-		System.out.println("Sum is: "+z);
+		System.out.println("hello world.");
+		
+		
 	}
 }
